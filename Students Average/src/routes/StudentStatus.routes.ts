31dc4b9bef19227632c0a2d15f925d908b830127
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2021-05-14 19:57:08 -0300

    refactor: adicionado interface das notas do aluno

--- Students Average/src/routes/StudentStatus.routes.ts
+++ Students Average/src/routes/StudentStatus.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import ReturnApprovalStatus from '../services/ReturnApprovalStatus';
+import { NotesData } from '../services/ReturnApprovalStatusDTO';
 
 const studentStatusRouter = Router();
 
 studentStatusRouter.post('/student-status', async (request, response) => {
   try {
-    return response.json(ReturnApprovalStatus.execute(request.body));
+    const notesData: NotesData = request.body;
+    return response.json(ReturnApprovalStatus.execute(notesData));
   } catch (error) {
     return response.json(error);
   }
