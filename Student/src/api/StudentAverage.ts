e5cabc54847370eaf45e00c3ce181b0263d5b678
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2021-05-16 16:14:36 -0300

    ci: configured port output

--- Student/src/api/StudentAverage.ts
+++ Student/src/api/StudentAverage.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const StudentAverage = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'http://studentaverage:4000',
 });
 
 export default StudentAverage;
